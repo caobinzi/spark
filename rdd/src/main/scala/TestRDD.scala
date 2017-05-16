dd40c0f5dfe10bff7653ec03da14b4d6e07c9146
Binzi Cao <caobinzi@gmail.com>
2017-05-16 15:36:51 +1000

    rdd test

---
+++ rdd/src/main/scala/TestRDD.scala
@@ -0,0 +1,37 @@
+import scala.language.higherKinds
+import scala.language.implicitConversions
+import scala.concurrent.duration.Duration
+import scala.util.{Try, Failure, Success}
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+case class Person(id: String, name: String)
+object RDDApp {
+  def run(sc: SparkContext) = {
+    while (true) {
+      val r = scala.util.Random
+      val data = (1 to r.nextInt(100)).toList.map { a =>
+        Person(a.toString, a.toString)
+      }
+      val rdd = sc.parallelize(data)
+      rdd.cache
+      println("running")
+      val a = (1 to 100).toList.map { x =>
+        Future(rdd.filter(_.id == x.toString).collect)
+      }
+      a.foreach { f =>
+        println(Await.ready(f, Duration.Inf).value.get)
+      }
+      rdd.unpersist()
+    }
+
+  }
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("test")
+    val sc   = new SparkContext(conf)
+    run(sc)
+
+  }
+}
