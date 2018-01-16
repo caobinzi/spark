a52c773b99e56c9723946231ede209a477841de1
Binzi Cao <binzic@campainmonitor.com>
2018-01-16 16:10:54 +1100

    rename files

--- learning/basic/src/main/scala/App.scala
+++ learning/basic/src/main/scala/App.scala
@@ -3,9 +3,6 @@ import scala.concurrent._
 import ExecutionContext.Implicits.global
 import scalaz._
 import Scalaz._
-class TaskError {
-  override def toString = "I will cause an error"
-}
 
 object MyApp {
   def getDataFrameFromJson(
@@ -28,14 +25,7 @@ object MyApp {
     val master = args.headOption
     val spark = getSparkSession("MyApp", master)
     val dataFrame = getDataFrameFromJson(spark, "people.json")
-    val taskError = new TaskError
-    dataFrame.foreach{
-      d =>
-        //println(taskError) //This will cause task serialization error
-        val error = new TaskError
-        println(error)
-    }
-
-    Thread.sleep(100000) //Sleep
+    dataFrame.show
+    Thread.sleep(100000)
   }
 }
--- learning/task/src/main/scala/App.scala
+++ learning/task/src/main/scala/App.scala
@@ -3,13 +3,21 @@ import scala.concurrent._
 import ExecutionContext.Implicits.global
 import scalaz._
 import Scalaz._
+import org.apache.spark.sql.functions._
+class TaskError {
+  override def toString = "I will cause an error"
+}
 
 object MyApp {
   def getDataFrameFromJson(
     spark:    SparkSession,
     jsonFile: String
   ): DataFrame = {
-    spark.read.json(jsonFile)
+
+    val dataFrame = spark.read.json(jsonFile)
+    val nameLength = udf((name: String) => name.length)
+    dataFrame.withColumn("NameLength", nameLength(col("name")))
+
   }
   def getSparkSession(
     appName: String,
@@ -21,10 +29,22 @@ object MyApp {
       none = spark
     ).getOrCreate
   }
+
   def main(args: Array[String]): Unit = {
     val master = args.headOption
     val spark = getSparkSession("MyApp", master)
+
     val dataFrame = getDataFrameFromJson(spark, "people.json")
+
+    val taskError = new TaskError
+    dataFrame.foreach{
+      d =>
+        //println(taskError) //This will cause task serialization error
+        val error = new TaskError
+        println(error)
+    }
     dataFrame.show
+
+    Thread.sleep(100000) //Sleep
   }
 }
